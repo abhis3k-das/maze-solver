5eb892de4fa9987d82955582a7182a17b406989b
abhis3k <abhis3k.das@gmail.com>
2023-03-08 21:05:14 +0530

    brought index outside

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require('path')
 const methodOverride = require('method-override');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }))
